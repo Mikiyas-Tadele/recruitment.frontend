451557f2f7680cd405758d485cc031793d3a039c
tesfay hishe <tesfayh09@gmail.com>
2021-07-23 15:27:02 +0300

    Apply vacancy form added

--- src/app/layout/apply-vacancy/apply-vacancy.component.ts
+++ src/app/layout/apply-vacancy/apply-vacancy.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Application } from 'src/app/models/application.model';
+import { RepositoryService } from 'src/app/models/services/repository.service';
 import { Userprofile } from 'src/app/models/userprofile.model';
 
 @Component({
@@ -9,20 +13,30 @@ import { Userprofile } from 'src/app/models/userprofile.model';
 })
 export class ApplyVacancyComponent implements OnInit {
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private route: ActivatedRoute, private repo: RepositoryService) { }
 
   applicationForm: FormGroup;
-  
-
+  vacancyId: number;
+  private routeSub: Subscription;
   ngOnInit() {
     this.applicationForm = this.fb.group({
       applicationLetter: ['', Validators.required]
     });
+    this.routeSub = this.route.params.subscribe(params => {
+       this.vacancyId = params['id'];
+    });
+  }
+  ngOnDestroy() {
+    this.routeSub.unsubscribe();
   }
   get applicationFormControl() {
     return this.applicationForm.controls;
   }
-  onSubmit({value, valid}: { value: Userprofile, valid: boolean }) {
+  onSubmit({value, valid}: { value: Application, valid: boolean }) {
+    if(valid) {
+      value.vacancyId = this.vacancyId;
+      this.repo.applyJob(value);
+    }
 
   }
 
--- src/app/models/services/repository.service.ts
+++ src/app/models/services/repository.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Application } from '../application.model';
 import { Userprofile } from '../userprofile.model';
 
 @Injectable({
@@ -10,9 +11,14 @@ export class RepositoryService {
   constructor(public httpClient: HttpClient, ) { }
 
   public apiUrl = '/api/';
+  url: string;
 saveApplication(profile: Userprofile, url: string) {
   url = this.apiUrl + url;
   this.sendRequest('POST', url, profile);
+}
+applyJob(application: Application) {
+  this.url = this.apiUrl + "/";
+  this.sendRequest('POST', this.url, application);
 }
   public sendRequest(method: string, url: string, options?: any) {
   return this.httpClient.request(method, url, options);
