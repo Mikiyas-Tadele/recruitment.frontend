7de8499ee45b1abd1a41ededd6f3af3f36a051a7
mikiyas <mikiyasbelay@gmail.com>
2021-07-23 09:47:41 +0300

    committing layout moudule config changes

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDropdownModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ToastModule } from 'primeng/toast';
 import {MessagesModule} from 'primeng/messages';
 import {MessageModule} from 'primeng/message';
@@ -16,7 +16,6 @@ import { HeaderComponent } from './components/header/header.component';
 import { VacancyPostComponent } from './vacancy-post/vacancy-post.component';
 import { ApplyVacancyComponent } from './apply-vacancy/apply-vacancy.component';
 import { PostVacancyComponent } from './post-vacancy/post-vacancy.component';
-import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 import { UserprofileComponent } from './userprofile/userprofile.component';
 import { VacancyViewComponent } from './vacancy-post/vacancy-view/vacancy-view.component';
 
@@ -26,13 +25,13 @@ import { VacancyViewComponent } from './vacancy-post/vacancy-view/vacancy-view.c
         CommonModule,
         LayoutRoutingModule,
         TranslateModule,
+        NgbModule,
         NgbDropdownModule,
         FormsModule,
         ReactiveFormsModule,
         ToastModule,
         MessagesModule,
         MessageModule,
-        NgbModule
     ],
     declarations: [LayoutComponent, SidebarComponent, HeaderComponent, VacancyPostComponent,
         ApplyVacancyComponent, PostVacancyComponent, UserprofileComponent, VacancyViewComponent],
